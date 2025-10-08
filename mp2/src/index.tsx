45142fcf7cf620900370aceb7089d09ff3e9f529
noemiamahmud <nmahm3@illinois.edu>
2025-10-07 19:18:33 -0500

    troubleshooting

--- mp2/src/index.tsx
+++ mp2/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/noemia_mp2">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
